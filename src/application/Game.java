4d4448dd8d104845d8bab34210ee6418e35dc340
Estupidani <megaman97ESP@gmail.com>
2016-12-27 11:03:59 +0100

    Initial commit

---
+++ src/application/Game.java
@@ -0,0 +1,11 @@
+package application;
+
+/**
+ * Created by estupidani on 27/12/16.
+ */
+public class Game {
+    public static void main(){
+        HangedMan hangedMan = new HangedMan();
+        hangedMan.play();
+    }
+}
